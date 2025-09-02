8c8ce939e0b4fee145be283cebcfdc4032d143a9
Thus spoke Zarathustra <zaratustra.business@gmail.com>
2025-09-02 09:19:39 -0300

    Line.py TERMINADO

--- line.py
+++ line.py
@@ -1,2 +1,30 @@
+import math
+
 def line():
-    print("TO DO")
+    coA = float(input("Ingrese el coeficiente A: "))
+    coB = float(input("Ingrese el coeficiente B: "))
+    coX1 = float(input("Ingrese el coeficiente X1: "))
+    coX2 = float(input("Ingrese el coeficiente X2: "))
+    coY1 = (coA * coX1) + coB
+    coY2 = (coA * coX2) + coB
+
+    coX3 = coX1
+    coY3 = coY2
+
+    dist = math.sqrt(((coX2 - coX3)**2) + ((coY1 - coY3)**2))
+
+    print(f"El coeficiente A de su ecuación de la recta es: {coA}")
+    print(f"El coeficiente A de su ecuación de la recta es: {coB}")
+    print(f"El coeficiente A de su ecuación de la recta es: {coX1}")
+    print(f"El coeficiente A de su ecuación de la recta es: {coX2}")
+
+    print("\nPara la siguiente ecuacion:")
+    print(f"\t Y = {coA}X + {coB}")
+
+    print("\nDados los siguientes puntos:")
+    print(f"\tP1 ({coX1}, {coY1})")
+    print(f"\tP1 ({coX2}, {coY2})") 
+
+    print(f"\nLa distancia entre ellos es: {dist}")
+
+line()
